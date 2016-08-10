3ff63de9327e594f8a5c005c90602174aead3788
DeXo98 <andreas-beck98@gmx.de>
2016-08-10 22:30:44 +0200

    Update fs_server.blade.php

--- src/views/fs_server.blade.php
+++ src/views/fs_server.blade.php
@@ -1,11 +1,13 @@
 <div class="container-fluid inspector-panel" id='panel-Server'>
 	<h3>$_SERVER</h3><br>
 	<table class="table table-striped">
-	@foreach($collectors['Server']['items'] as $key=>$value)
-		<tr>
-			<td><strong>{{$key}}</strong></td>
-			<td>{{$value}}</td>
-		</tr>
-	@endforeach
+	@if (isset($collectors['Server']) && isset($collectors['Server']['items']))
+		@foreach($collectors['Server']['items'] as $key=>$value)
+			<tr>
+				<td><strong>{{$key}}</strong></td>
+				<td>{{$value}}</td>
+			</tr>
+		@endforeach
+	@endif
 	</table>
 </div>
