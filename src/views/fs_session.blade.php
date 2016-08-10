65820d91ef5e0d694cc4d64c826901021c2222c6
DeXo98 <andreas-beck98@gmx.de>
2016-08-10 22:30:57 +0200

    Update fs_session.blade.php

--- src/views/fs_session.blade.php
+++ src/views/fs_session.blade.php
@@ -1,14 +1,16 @@
 <div class="container-fluid inspector-panel" id='panel-Session'>
 	<h3>Session</h3>
-	@foreach($collectors['Session']['items'] as $key=>$value)
-	<div style="border:1px solid #ddd; box-shadow: 0px 0px 2px #ccc;">
-		<div style="border-bottom:1px solid #ddd; padding:5px">
-			<strong>{{$key}}</strong>
+	@if (isset($collectors['Session']) && isset($collectors['Session']['items']))
+		@foreach($collectors['Session']['items'] as $key=>$value)
+		<div style="border:1px solid #ddd; box-shadow: 0px 0px 2px #ccc;">
+			<div style="border-bottom:1px solid #ddd; padding:5px">
+				<strong>{{$key}}</strong>
+			</div>
+			<div>
+				<pre style="background: #fff; color: #c7254e; font-size:15px;border:0">{!! inspector()->getDump($value) !!}</pre>
+			</div>
 		</div>
-		<div>
-			<pre style="background: #fff; color: #c7254e; font-size:15px;border:0">{!! inspector()->getDump($value) !!}</pre>
-		</div>
-	</div>
-	<br>
-	@endforeach
+		<br>
+		@endforeach
+	@endif
 </div>
